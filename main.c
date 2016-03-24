2da66974b9f8f4262a3be78d1308b3b1ed44fa2f
Claire <claire.durand@temple.edu>
2016-03-24 16:21:22 -0400

    working generateCodeword function

--- main.c
+++ main.c
@@ -38,8 +38,7 @@
 #define FlipBit(var, bit) (var ^= (1 << bit))
 
 void DelayMsec(unsigned int);
-void generateParity(char text[], int len);
-int compareParity(char packet[], int plen); // return number of detected errors
+char generateCodeword(char p);
 
 int main() {
     int rlen, tlen, i;
@@ -56,32 +55,16 @@ int main() {
     int delayT = 0;
 
     // Declare the arrays for the full text to be transmitted
-    char fullText1[7] = "claire";
-    char fullText2[7] = "durand";
-    char fullText3[7] = "aaabbb";
-    char fullText4[7] = "cccddd";
-    char fullText5[7] = "eeefff";
-    int fullLen = strlen(fullText1) + 1; // Obtain length of fullText1 array
+    char fullText[18] = "EE is my avocation";
+    int fullLen = strlen(fullText) + 1; // Obtain length of fullText1 array
+    // add one because strlen does not include the null terminator
+    
+    char parpack = 4;   // Test case
+    char codeword;
+    codeword = generateCodeword(parpack);
 
     // CREATE PARITY MATRICES HERE
-    generateParity(fullText1, fullLen);
-    generateParity(fullText2, fullLen);
-    generateParity(fullText3, fullLen);
-    generateParity(fullText4, fullLen);
-    generateParity(fullText5, fullLen);
-
-    // Form the full output transmission
-    char fullText[36];
-    int t;
-    for (t = 0; t < fullLen; t++) {
-        fullText[t] = fullText1[t];
-        fullText[t + fullLen] = fullText2[t];
-        fullText[t + (2 * fullLen)] = fullText3[t];
-        fullText[t + (3 * fullLen)] = fullText4[t];
-        fullText[t + (4 * fullLen)] = fullText5[t];
-    }
-    fullText[35] = 0x00;
-
+    //generateParity(fullText, fullLen);
 
     // LED setup
     mPORTDSetPinsDigitalOut(BIT_0 | BIT_1 | BIT_2); // RD0, RD1 and RD2 as outputs
@@ -117,7 +100,6 @@ int main() {
     listen(srvr, 5);
 
     while (1) {
-
         IP_ADDR ip;
         TCPIPProcess();
         DHCPTask();
@@ -134,7 +116,6 @@ int main() {
             if (StreamSock != INVALID_SOCKET) {
                 tlen = 1296; // send buffer size
                 setsockopt(StreamSock, SOL_SOCKET, SO_SNDBUF, (char*) &tlen, sizeof (int));
-                //setsockopt(StreamSock, IPPROTO_TCP, TCP_NODELAY, (char*) &tlen, sizeof (int));
             }
         } else {
             // receive TCP data
@@ -148,57 +129,10 @@ int main() {
                         DelayMsec(50);
                         mPORTDClearBits(BIT_0); // LED1=0
                     }
-                } else {
+                } else { // Receiving "other" message
                     // Blink lights for mood
                     mPORTDSetBits(BIT_0); // LED2=1
-                    DelayMsec(1000);
-                    mPORTDClearBits(BIT_0); // LED2=1
-                    mPORTDSetBits(BIT_1); // LED2=1
-                    DelayMsec(1000);
-                    mPORTDClearBits(BIT_1); // LED2=1
-                    mPORTDSetBits(BIT_2); // LED2=1
-                    DelayMsec(1000);
-                    mPORTDClearBits(BIT_2); // LED2=1
 
-                    char rcvdTxt1[8];
-                    char rcvdTxt2[8];
-                    char rcvdTxt3[8];
-                    char rcvdTxt4[8];
-                    char rcvdTxt5[8];
-                    char colRcvdPar[8];
-
-                    // Break up datagram into packets
-                    int r;
-                    for (r = 0; r < 7; r++) {
-                        rcvdTxt1[r] = rbfr[r];
-                        rcvdTxt2[r] = rbfr[r + 7];
-                        rcvdTxt3[r] = rbfr[r + 14];
-                        rcvdTxt4[r] = rbfr[r + 21];
-                        rcvdTxt5[r] = rbfr[r + 28];
-                        //colRcvdPar[r] = rbfr[r + 35];    
-                    }
-
-                    int err1, err2, err3, err4, err5;
-
-                    err1 = compareParity(rcvdTxt1, 7);
-                    err2 = compareParity(rcvdTxt2, 7);
-                    err3 = compareParity(rcvdTxt3, 7);
-                    err4 = compareParity(rcvdTxt4, 7);
-                    err5 = compareParity(rcvdTxt5, 7);
-
-                    // Generate new parity and XOR it with rcvd parity
-                    // Re-generate + compare parities
-                    
-                    if(err1 > 0 || err2 > 0 || err3 > 0 || err4 > 0 || err5>0 ){
-                        send(StreamSock, tbfr, tlen + 1, 0);
-                    }
-
-                    mPORTDSetBits(BIT_0); // LED2=1
-                    DelayMsec(1000);
-                    mPORTDClearBits(BIT_0); // LED2=1
-                    mPORTDSetBits(BIT_1); // LED2=1
-                    DelayMsec(1000);
-                    mPORTDClearBits(BIT_1); // LED2=1
                 }
 
                 if (rbfr[1] == 84) //T transfer
@@ -230,7 +164,7 @@ int main() {
 
                     // Copy the long text into the transmit buffer
                     for (j = 0; j < tlen; j++) {
-                        tbfr[j] = fullText1[j];
+                        tbfr[j] = fullText[j];
                     }
 
                     send(StreamSock, tbfr, tlen, 0);
@@ -255,7 +189,6 @@ int main() {
 } // end
 
 // DelayMsec( )   software millisecond delay
-
 void DelayMsec(unsigned int msec) {
     unsigned int tWait, tStart;
 
@@ -264,101 +197,25 @@ void DelayMsec(unsigned int msec) {
     while ((ReadCoreTimer() - tStart) < tWait);
 }
 
-// CREATING PARITY MATRIX JAMS
+// CREATING LINEAR BLOCK CODE JAMS
 
-void generateParity(char text[], int len) {
-    // Declare index variables for the byte and bit 
-    int by, bi;
-    int colParity = 0;
-    int rowParity;
-
-    // Obtain parity matrix
-    for (by = 0; by < len - 1; by++) {
-        // Shift every byte by 1 to make room for parity
-        text[by] = text[by] << 1;
-        rowParity = 0; // Reset rowParity for each byte
-
-        // iterate through the bits of each byte (skipping least sign bit)
-        for (bi = 1; bi < 8; bi++) {
-            // Delay added because MPLABX can't handle this
-            DelayMsec(10);
-            if (GetBit(text[by], bi)) {
-                rowParity++;
-                FlipBit(text[6], bi); // Actually colParity
-            } // else bit is not set, do nothing
-            DelayMsec(10);
-        } // end of bits for loop
-
-        // End of byte, calculate and set the parity bit
-        if (rowParity % 2 == 0) { // even parity 
-            ClearBit(text[by], 0);
-        } else { // odd parity
-            SetBit(text[by], 0);
-            colParity++;
-        }
-    } // end of bytes for loop
-
-    if (colParity % 2 == 0) { // even parity     
-        ClearBit(text[6], 0);
-    } else { // odd parity
-        SetBit(text[6], 0);
+char generateCodeword(char p){
+    int i;
+    char bitVal;
+    char x = 0; // implicit zero-stuffing
+    
+    char Gb0 = 13;  // 00001101
+    char Gb1 = 23;  // 00010111
+    char Gb2 = 38;  // 00100110
+    // 6 because of the (6,3) LBC
+    for (i=0; i<6; i++){
+        bitVal = ((GetBit(p,2))&(GetBit(Gb2,i)))^((GetBit(p,1))&(GetBit(Gb1,i)))^((GetBit(p,0))&(GetBit(Gb0,i)));
+        
+        if(bitVal == 1)
+            SetBit(x,i);
+        // else if bitVal == 0, the bit is already clear
     }
-    // END OF CREATING PARITY MATRIX JAMS  
+    
+    return x;
 }
 
-
-// 
-
-int compareParity(char packet[], int plen) {
-    // Declare index variables for the byte and bit 
-    int by, bi;
-    int error = 0; // Assume no error
-    int rowNewP = 0;
-    char colNewP = 0x00;
-
-    char p1 = packet[0];
-    char p2 = packet[1];
-    char p3 = packet[2];
-    char p4 = packet[3];
-    char p5 = packet[4];
-    char p6 = packet[5];
-    char cp = packet[6];
-
-    // Obtain parity matrix
-    for (by = 0; by < plen - 1; by++) {
-        rowNewP = 0; // Reset rowParity for each byte
-
-        // iterate through the bits of each byte (skipping least sign bit)
-        for (bi = 0; bi < 8; bi++) {
-            // Delay added because MPLABX can't handle this
-            DelayMsec(10);
-            if (GetBit(packet[by], bi)) {
-                if (bi != 0) {
-                    rowNewP++;
-                }
-                FlipBit(colNewP, bi); // Actually colParity
-            } // else bit is not set, do nothing
-            DelayMsec(10);
-        } // end of bits for loop
-
-        if (bi > 0) {
-            // End of byte, calculate and set the parity bit
-            // if newP is even and old is odd OR newP is odd and old is even
-            if (((rowNewP % 2 == 0) && (GetBit(packet[by], 0))) || ((rowNewP % 2 == 1) && !(GetBit(packet[by], 0)))) {
-                // Should record the coordinates 
-                error++;
-            } else { // odd parity
-                // SUCCESSFUL ROW
-            }
-        }
-    } // end of bytes for loop
-
-    if ((colNewP ^ packet[6]) == 0x00) {
-        // We're Gucci
-    } else {
-        // ERROR. Need to loop through to find x coordinate
-        error++;
-    }
-
-    return error;
-}
